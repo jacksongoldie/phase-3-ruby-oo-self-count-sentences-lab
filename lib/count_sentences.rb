88b2e0c362cff8a03a8752fcc52016a438d07b29
Goldie <goldiejackson25@yahoo.com>
2021-11-24 11:57:02 -0600

    complete

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,27 @@
+#practice of adding methods to or altering Ruby's built in classes is called monkey patching == BAD
+
 require 'pry'
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    split = self.split(/(\.|\?|\!)/)
+    removed = split.filter {|s| s.length > 2}
+    removed.count
   end
 end
+
+s1 = String.new("This, well, is a sentence. This is too!! And so is this, I think? Woo...")
+puts s1.count_sentences
